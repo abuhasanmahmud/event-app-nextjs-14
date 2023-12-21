9a9e744d5efcf13a3dc1ba0899c5413e9ad6646b
abuhasanmahmud <mahmud.me420@gmail.com>
2023-12-21 22:31:01 +0600

    s

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -18,8 +19,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ components/shared/NavItems.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavItems = () => {
+  return <div>navitems</div>;
+};
+
+export default NavItems;

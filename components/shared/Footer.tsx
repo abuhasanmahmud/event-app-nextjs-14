317f141d5a2bd5cd02a7e1fffc9aa37b4681d378
abuhasanmahmud <mahmud.me420@gmail.com>
2023-12-25 22:14:29 +0600

    ss

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Footer = () => {
-  console.log("s");
+  // console.log("s");
   return (
     <footer className="border-t">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
